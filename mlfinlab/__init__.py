23ae793a82bb15a937c1b95750380e00fe9b7405
Garth Trickett <garthtrickett_gmail_com@spartan-ai-cpu.asia-east1-c.c.crafty-plateau-241306.internal>
2020-02-04 04:06:23 +0000

    removed the init import module as names

--- mlfinlab/__init__.py
+++ mlfinlab/__init__.py
@@ -2,18 +2,19 @@
 Package based on the text book: Advances in Financial Machine Learning, by Marcos Lopez de Prado
 """
 
-import mlfinlab.cross_validation as cross_validation
-import mlfinlab.data_structures as data_structures
-import mlfinlab.multi_product as multi_product
-import mlfinlab.filters.filters as filters
-import mlfinlab.labeling.labeling as labeling
-import mlfinlab.features.fracdiff as fracdiff
-import mlfinlab.sample_weights as sample_weights
-import mlfinlab.sampling as sampling
-import mlfinlab.bet_sizing as bet_sizing
-import mlfinlab.util as util
-import mlfinlab.structural_breaks as structural_breaks
-import mlfinlab.feature_importance as feature_importance
-import mlfinlab.ensemble as ensemble
-import mlfinlab.portfolio_optimization as portfolio_optimization
-import mlfinlab.clustering as clustering
+
+# import mlfinlab.cross_validation as cross_validation
+# import mlfinlab.data_structures as data_structures
+# import mlfinlab.multi_product as multi_product
+# import mlfinlab.filters.filters as filters
+# import mlfinlab.labeling.labeling as labeling
+# import mlfinlab.features.fracdiff as fracdiff
+# import mlfinlab.sample_weights as sample_weights
+# import mlfinlab.sampling as sampling
+# import mlfinlab.bet_sizing as bet_sizing
+# import mlfinlab.util as util
+# import mlfinlab.structural_breaks as structural_breaks
+# import mlfinlab.feature_importance as feature_importance
+# import mlfinlab.ensemble as ensemble
+# import mlfinlab.portfolio_optimization as portfolio_optimization
+# import mlfinlab.clustering as clustering
